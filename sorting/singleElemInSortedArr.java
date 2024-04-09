c014830b074e6532f580d47abc5db03fae843b79
Ansh-1904 <solankiansh3693@gmail.com>
2024-04-09 14:08:33 +0530

    Optimal Approach Added

--- sorting/singleElemInSortedArr.java
+++ sorting/singleElemInSortedArr.java
@@ -1,26 +1,38 @@
-//Brute force approach
-//T.C O(N)
-class singleElemInSortedArr {
-    public static int singleNonDuplicate(int[] arr) {
-        if(arr.length==1)
+//Optimal Approach
+//T.C O(LogN)
+class singleElemInSortedArr
+{    
+    public static int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+
+        if(nums[0]!=nums[1]) 
         {
-            return arr[0];
+            return nums[0];
         }
-        for(int i=0;i<arr.length;i++)
+
+        if(nums[n-1]!=nums[n-2])
         {
-            if(i==0)
+            return nums[n-1];
+        }
+
+        int low=1;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
             {
-                if(arr[i]!=arr[i+1]) return arr[i];
-            }
-            else if(i==arr.length-1){
-                if(arr[i]!=arr[i-1]) return arr[i];
-            }
+                low=mid+1;
+            } 
             else{
-                if(arr[i]!=arr[i-1] && arr[i]!=arr[i+1]) return arr[i];
+                high=mid-1;
             }
         }
         return -1;
     }
+    
 
     public static void main(String[] args) {
         int arr[]={1,1,2,3,3,4,4,8,8};
@@ -29,3 +41,36 @@ class singleElemInSortedArr {
         System.out.println(ans);
     }
 }
+
+
+//Brute force approach
+//T.C O(N)
+// class singleElemInSortedArr {
+//     public static int singleNonDuplicate(int[] arr) {
+//         if(arr.length==1)
+//         {
+//             return arr[0];
+//         }
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             if(i==0)
+//             {
+//                 if(arr[i]!=arr[i+1]) return arr[i];
+//             }
+//             else if(i==arr.length-1){
+//                 if(arr[i]!=arr[i-1]) return arr[i];
+//             }
+//             else{
+//                 if(arr[i]!=arr[i-1] && arr[i]!=arr[i+1]) return arr[i];
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[]={1,1,2,3,3,4,4,8,8};
+//         // int arr[] = {3, 3, 7, 7, 10, 11, 11};
+//         int ans = singleNonDuplicate(arr);
+//         System.out.println(ans);
+//     }
+// }
